95f62d32eb9b759ac3eabbd7cdfbebcef8cb7510
Joseph Vasquez <joetv.tech@gmail.com>
2021-07-03 15:08:38 -0700

    Added some styling and custom shape attributes

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const canvas = new fabric.Canvas("canvas");
 
+console.log("canvas", canvas);
+
+// Set Default Shapes attributes
+const shapes = { width: 40, height: 40, fill: "#00a2ff", stroke: "#005688" };
+
 const rect = new fabric.Rect({
-  left: 100,
-  right: 100,
-  fill: "#232323",
-  width: 20,
-  height: 20,
+  top: 40,
+  left: (canvas.width / 2 - shapes.width / 2),
+  fill: shapes.fill,
+  stroke: shapes.stroke,
+  width: shapes.width,
+  height: shapes.height,
 });
+
+console.log("rect", rect);
 
 canvas.add(rect);
