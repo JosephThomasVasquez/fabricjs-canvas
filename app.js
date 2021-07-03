ffc9459a4a48d43efa9114a2f8d1861382f760f9
Joseph Vasquez <joetv.tech@gmail.com>
2021-07-03 13:17:41 -0700

    Removed unnecessary modules

---
+++ app.js
@@ -0,0 +1,11 @@
+const canvas = new fabric.Canvas("canvas");
+
+const rect = new fabric.Rect({
+  left: 100,
+  right: 100,
+  fill: "#232323",
+  width: 20,
+  height: 20,
+});
+
+canvas.add(rect);
