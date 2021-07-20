866e80158af8ec5039a55b49ed127b623fd2a497
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-07-19 21:37:44 -0700

    Added form elements

--- app.js
+++ app.js
@@ -6,42 +6,49 @@ fabric.Object.prototype.noScaleCache = false;
 	Objects in group are not scaled directly, so stroke uniform will not have effect.
 */
 function toggleUniform() {
-	var aObject = canvas.getActiveObject();
-	if (aObject.type === 'activeSelection') {
-		aObject.getObjects().forEach(function(obj) {
-			obj.set('strokeUniform', !obj.strokeUniform);
-		});
-	} else {
-		aObject.set('strokeUniform', !aObject.strokeUniform);
-	}
-	canvas.requestRenderAll();
+  var aObject = canvas.getActiveObject();
+  if (aObject.type === "activeSelection") {
+    aObject.getObjects().forEach(function (obj) {
+      obj.set("strokeUniform", !obj.strokeUniform);
+    });
+  } else {
+    aObject.set("strokeUniform", !aObject.strokeUniform);
+  }
+  canvas.requestRenderAll();
 }
 
 console.log("canvas", canvas);
 
 // Set Default Shapes attributes
-const shapes = { width: 40, height: 40, fill: "#00a2ff", stroke: "#005688", strokeWidth: 2 };
+const shapes = {
+  width: 80,
+  height: 40,
+  fill: "#00a2ff",
+  stroke: "#005688",
+  strokeWidth: 2,
+};
 
 const rect = new fabric.Rect({
   top: 40,
-  left: (canvas.width / 2 - shapes.width / 2),
+  left: canvas.width / 2 - shapes.width / 2,
   fill: shapes.fill,
   stroke: shapes.stroke,
   strokeWidth: shapes.strokeWidth,
   width: shapes.width,
   height: shapes.height,
-  strokeUniform: true
+  strokeUniform: true,
+  cornerColor: "#232323",
 });
 
 const rect2 = new fabric.Rect({
   top: 40,
-  left: (canvas.width / 2 + 100),
+  left: canvas.width / 2 + 100,
   fill: "#ff00a2",
   stroke: "#885600",
   strokeWidth: shapes.strokeWidth,
   width: shapes.width,
   height: shapes.height,
-  strokeUniform: true
+  strokeUniform: true,
 });
 
 console.log("rect", rect);
